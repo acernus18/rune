70ae94f88e7c79ec3ef57e8900957d1a1d04ad27
Maples <32149535+acernus18@users.noreply.github.com>
2025-04-12 16:19:25 +0700

    [Feature]: working on request and reply

--- index.js
+++ index.js
@@ -1,1 +1,117 @@
-console.log("hello, world");
+"use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var Rune;
+(function (Rune) {
+    class Exception extends Error {
+        constructor(code, name, message) {
+            super(message);
+            this._code = code;
+            this._name = name;
+        }
+        get code() {
+            return this._code;
+        }
+        get name() {
+            return this._name;
+        }
+        toString() {
+            return `[${this._name}-${this._code}]: ${this.message}`;
+        }
+    }
+    Rune.Exception = Exception;
+    class RequestContext {
+        constructor(sn, sid, aid, cmd, data) {
+            this.aid = aid;
+            this.cmd = cmd;
+            this.data = data;
+            this.sid = sid;
+            this.sn = sn;
+        }
+    }
+    Rune.RequestContext = RequestContext;
+    class ResponseContext {
+        constructor(sn, code, message, data) {
+            this.code = code;
+            this.data = data;
+            this.message = message;
+            this.sn = sn;
+        }
+        static success(sn, data) {
+            return new Rune.ResponseContext(sn, 0, "SUC", data !== null && data !== void 0 ? data : null);
+        }
+        static exception(sn, err) {
+            return new Rune.ResponseContext(sn, err.code, err.toString(), null);
+        }
+        static systemError(sn) {
+            return new Rune.ResponseContext(sn, -1, "[SystemErr-(-1)]: system error.", null);
+        }
+    }
+    Rune.ResponseContext = ResponseContext;
+    class ServicesRouter {
+        constructor(sessionProvider) {
+            this.sessionProvider = sessionProvider;
+            this.serviceProvider = new Map();
+        }
+        addService(id, cmd, service) {
+            let appServices = this.serviceProvider.get(id);
+            if (!appServices) {
+                appServices = new Map();
+            }
+            appServices.set(cmd, service);
+            this.serviceProvider.set(id, appServices);
+        }
+        reply(req) {
+            return __awaiter(this, void 0, void 0, function* () {
+                const [session, err] = yield this.sessionProvider(req.sid);
+                if (err !== null) {
+                    return ResponseContext.exception(req.sn, err);
+                }
+                if (session === null) {
+                    return ResponseContext.systemError(req.sn);
+                }
+                const appServices = this.serviceProvider.get(req.aid);
+                if (!appServices) {
+                    return ResponseContext.systemError(req.sn);
+                }
+                const service = appServices.get(req.cmd);
+                if (!service) {
+                    return ResponseContext.systemError(req.sn);
+                }
+                const [serviceResult, serviceErr] = yield service(session, req);
+                if (serviceErr !== null) {
+                    return ResponseContext.exception(req.sn, serviceErr);
+                }
+                return ResponseContext.success(req.sn, serviceResult);
+            });
+        }
+    }
+    Rune.ServicesRouter = ServicesRouter;
+})(Rune || (Rune = {}));
+const handler = new Rune.ServicesRouter(() => new Promise(resolve => resolve([1, null])));
+handler.addService("1", "1", (number, any) => {
+    return new Promise(resolve => resolve([1, null]));
+});
+// function Handler(aid: string, cmd: string) {
+//     return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+//         console.log(`[${propertyKey}] ${descriptor}`);
+//     };
+// }
+//
+// @Handler("2", "2")
+// function service(session: number, req: any) {
+//     return new Promise(resolve => resolve([1, null]));
+// }
+(() => __awaiter(void 0, void 0, void 0, function* () {
+    const r = yield handler.reply({
+        sn: "1", cmd: "1", aid: "1", data: null, sid: "1"
+    });
+    console.log(r);
+}))();
--- index.ts
+++ index.ts
@@ -1,1 +1,168 @@
-console.log("hello, world");
+namespace Rune {
+    export class Exception extends Error {
+        private readonly _code: number;
+        private readonly _name: string;
+
+        constructor(code: number, name: string, message: string) {
+            super(message);
+            this._code = code;
+            this._name = name;
+        }
+
+        get code(): number {
+            return this._code;
+        }
+
+        get name(): string {
+            return this._name;
+        }
+
+        toString(): string {
+            return `[${this._name}-${this._code}]: ${this.message}`;
+        }
+    }
+
+    // export class Logger {
+    //     public constructor() {
+    //     }
+    //
+    //     public debug(message: string): void {
+    //     }
+    //
+    //     public info(message: string): void {
+    //     }
+    //
+    //     public warn(message: string): void {
+    //     }
+    //
+    //     public error(message: string): void {
+    //     }
+    // }
+
+    export interface RequestProtocol {
+        sn: string;
+        aid: string;
+        cmd: string;
+        sid: string;
+        data: any;
+    }
+
+    export interface ResponseProtocol {
+        sn: string;
+        code: number;
+        message: string;
+        data: any;
+    }
+
+    export class RequestContext implements RequestProtocol {
+        public aid: string;
+        public cmd: string;
+        public data: any;
+        public sid: string;
+        public sn: string;
+
+        constructor(sn: string, sid: string, aid: string, cmd: string, data: any) {
+            this.aid = aid;
+            this.cmd = cmd;
+            this.data = data;
+            this.sid = sid;
+            this.sn = sn;
+        }
+    }
+
+    export class ResponseContext implements ResponseProtocol {
+        public code: number;
+        public data: any;
+        public message: string;
+        public sn: string;
+
+        constructor(sn: string, code: number, message: string, data: any) {
+            this.code = code;
+            this.data = data;
+            this.message = message;
+            this.sn = sn;
+        }
+
+        public static success(sn: string, data?: any): ResponseProtocol {
+            return new Rune.ResponseContext(sn, 0, "SUC", data ?? null);
+        }
+
+        public static exception(sn: string, err: Exception): ResponseProtocol {
+            return new Rune.ResponseContext(sn, err.code, err.toString(), null);
+        }
+
+        public static systemError(sn: string): ResponseProtocol {
+            return new Rune.ResponseContext(sn, -1, "[SystemErr-(-1)]: system error.", null);
+        }
+    }
+
+    type Result<T> = [T | null, Exception | null];
+    type AsyncResult<T> = Promise<Result<T>>;
+    type Service<SESSION, RESULT> = (session: SESSION, request: RequestProtocol) => AsyncResult<RESULT>;
+
+    export class ServicesRouter<SESSION> {
+        private readonly sessionProvider: (id: string) => AsyncResult<SESSION>;
+        private readonly serviceProvider: Map<string, Map<string, Service<SESSION, any>>>;
+
+        public constructor(sessionProvider: (id: string) => AsyncResult<SESSION>) {
+            this.sessionProvider = sessionProvider;
+            this.serviceProvider = new Map<string, Map<string, Service<SESSION, any>>>();
+        }
+
+        public addService(id: string, cmd: string, service: Service<SESSION, any>): void {
+            let appServices = this.serviceProvider.get(id);
+            if (!appServices) {
+                appServices = new Map<string, Service<SESSION, any>>();
+            }
+            appServices.set(cmd, service);
+            this.serviceProvider.set(id, appServices);
+        }
+
+        public async reply(req: RequestProtocol): Promise<ResponseProtocol> {
+            const [session, err] = await this.sessionProvider(req.sid);
+            if (err !== null) {
+                return ResponseContext.exception(req.sn, err);
+            }
+            if (session === null) {
+                return ResponseContext.systemError(req.sn);
+            }
+            const appServices = this.serviceProvider.get(req.aid);
+            if (!appServices) {
+                return ResponseContext.systemError(req.sn);
+            }
+            const service = appServices.get(req.cmd);
+            if (!service) {
+                return ResponseContext.systemError(req.sn);
+            }
+            const [serviceResult, serviceErr] = await service(session, req);
+            if (serviceErr !== null) {
+                return ResponseContext.exception(req.sn, serviceErr);
+            }
+            return ResponseContext.success(req.sn, serviceResult);
+        }
+    }
+}
+
+const handler = new Rune.ServicesRouter<number>(() => new Promise<[number, null]>(resolve => resolve([1, null])));
+
+handler.addService("1", "1", (number, any) => {
+    return new Promise(resolve => resolve([1, null]));
+});
+
+// function Handler(aid: string, cmd: string) {
+//     return function (target: any, propertyKey: string, descriptor: PropertyDescriptor) {
+//         console.log(`[${propertyKey}] ${descriptor}`);
+//     };
+// }
+//
+// @Handler("2", "2")
+// function service(session: number, req: any) {
+//     return new Promise(resolve => resolve([1, null]));
+// }
+
+(async () => {
+    const r = await handler.reply({
+        sn: "1", cmd: "1", aid: "1", data: null, sid: "1"
+    });
+    console.log(r);
+})();
